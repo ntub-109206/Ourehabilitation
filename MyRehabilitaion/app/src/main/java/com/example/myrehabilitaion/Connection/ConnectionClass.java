510674ad46e38c75a5aed07f34d5f82ff729f2f6
Vincent-Chung-SpecialTopic <10836025@ntub.edu.tw>
2020-12-01 16:49:31 +0800

    重新上傳基本版

--- MyRehabilitaion/app/src/main/java/com/example/myrehabilitaion/Connection/ConnectionClass.java
+++
@@ -1,8 +0,0 @@
-package com.example.myrehabilitaion.Connection;
-
-public class ConnectionClass {
-    public static String ip = "140.131.114.241";
-    public static String un = "109-rehabilitation";
-    public static String pass = "case210906";
-    public static String db = "1@case206";
-}
